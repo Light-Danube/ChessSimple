7f8a33890b0143b9f39caf9faa1f7805a020f6bf
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-13 19:17:17 +0300

    Update pawn.py

--- chess_data/pieces/pawn.py
+++ chess_data/pieces/pawn.py
@@ -1,5 +1,12 @@
+import pygame
+
 class Pawn:
-    # ...
+    def __init__(self, color, pos, img_path):
+        self.color = color
+        self.pos = pos
+        self.img_path = pygame.image.load(img_path)
+        self.SQUARE_SIZE = self.image.get_width()
+    
     def get_valid_moves(self, board):
         valid_moves = []
         row, col = self.position
@@ -18,6 +25,8 @@ class Pawn:
                 valid_moves.append((row + 1, col))
                 if row == 1 and board[3][col] is None:
                     valid_moves.append((3, col))
+            
+            # Diagonaly captures:
             if row < 7 and col > 0 and board[row + 1][col - 1] is not None and board[row + 1][col - 1].color == 'white':
                 valid_moves.append((row + 1, col - 1))
             if row < 7 and col < 7 and board[row + 1][col + 1] is not None and board[row + 1][col + 1].color == 'white':
