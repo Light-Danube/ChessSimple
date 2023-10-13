59ac1f6d2d58741de13d79a32f0bda1a3ac040f4
Light-Danube <71426440+Light-Danube@users.noreply.github.com>
2023-10-14 01:30:22 +0300

    Update on Moving & Drawing

--- chess_data/board.py
+++ chess_data/board.py
@@ -1,4 +1,5 @@
 import pygame
+from chess_data.square import Square
 from chess_data.pieces.pawn import Pawn
 from chess_data.pieces.rook import Rook
 from chess_data.pieces.queen import Queen
@@ -13,128 +14,59 @@ class Chessboard:
         self.board = [[None for _ in range(8)] for _ in range(8)]
         self.selected_piece = None
         self.valid_moves = []
-        self.turn = 'white'  # Add a turn attribute
+        self.squares = self.generate_squares()
 
-        # Define the initial configuration
-        initial_config = [
-            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
-            ['', '', '', '', '', '', '', ''],
-            ['', '', '', '', '', '', '', ''],
-            ['', '', '', '', '', '', '', ''],
-            ['', '', '', '', '', '', '', ''],
-            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
-        ]
-        
-        self.setup_board(initial_config)
-    
-    def create_piece(self, piece_code, position):
-        color = 'white' if piece_code[0] == 'w' else 'black'
-        piece_type = piece_code[1]
+    def generate_squares(self):
+        squares = []
+        for y in range(8):
+            for x in range(8):
+                square = Square(x, y, self.SQUARE_SIZE, self.SQUARE_SIZE)
+                squares.append(square)
+        return squares
 
-        # Implement the creation of piece objects based on piece_code
-        if piece_type == 'P':
-            return Pawn(color, position)
-        if piece_type == "R":
-            return Rook(color, position)
-        if piece_type == "Q":
-            return Queen(color, position)
-        if piece_type == "K":
-            return King(color, position)
-        if piece_type == "B":
-            return Bishop(color, position)
-        if piece_type == "N":
-            return Knight(color, position)
-    
-    def setup_board(self, config):
-        for row in range(8):
-            for col in range(8):
-                piece_code = config[row][col]
-                if piece_code:
-                    piece = self.create_piece(piece_code, (row, col))
-                    self.board[row][col] = piece
-
-    
     def draw(self):
-        for row in range(8):
-            for col in range(8):
-                color = (200, 200, 200) if (row + col) % 2 == 0 else (100, 100, 100)
-                pygame.draw.rect(self.screen, color, (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE, self.SQUARE_SIZE, self.SQUARE_SIZE))
-                
-                piece = self.board[row][col]
-                if piece:
-                    piece_image = piece.image
-
-                    # Resize the image to fit within the 50x50 pixel square
-                    piece_image = pygame.transform.scale(piece_image, (self.SQUARE_SIZE, self.SQUARE_SIZE))
-
-                    # Determine the offset to center the image within the square
-                    x_offset = (self.SQUARE_SIZE - piece_image.get_width()) // 2
-                    y_offset = (self.SQUARE_SIZE - piece_image.get_height()) // 2
-
-                    # Calculate the position to blit the image on the screen
-                    blit_position = (col * self.SQUARE_SIZE + x_offset, row * self.SQUARE_SIZE + y_offset)
-                    
-                    if piece == self.selected_piece:
-                        pygame.draw.rect(self.screen, (0, 255, 0), (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE, self.SQUARE_SIZE, self.SQUARE_SIZE), 4)
-                    
-                    if (row, col) in self.valid_moves:
-                        pygame.draw.rect(self.screen, (0, 0, 255), (col * self.SQUARE_SIZE, row * self.SQUARE_SIZE, self.SQUARE_SIZE, self.SQUARE_SIZE), 4)
-                        print(f"Valid moves for {self.color} {self.__class__.__name__} at {self.position}: {self.valid_moves}")
-
-
-
-                    self.screen.blit(piece_image, blit_position)
+        for square in self.squares:
+            square.draw(self.screen)
 
     def handle_click(self, position, player_color):
         row, col = position
-        piece = self.board[row][col]
+        square = self.get_square_from_pos(position)
 
-
-        print("Clicked at", position, "Piece:", piece)  # Add this line
-
-        if piece and piece.color == player_color:
-            if piece == self.selected_piece:
-                # Deselect the piece if it's already selected
+        if square.occupying_piece and square.occupying_piece.color == player_color:
+            if square.occupying_piece == self.selected_piece:
                 self.selected_piece = None
                 self.valid_moves = []
             else:
-                # Select the piece if it's not selected
-                self.selected_piece = piece
+                self.selected_piece = square.occupying_piece
                 self.valid_moves = self.get_valid_moves(player_color)
         elif self.selected_piece:
-            if position in self.valid_moves:
-                # Call the move function to move the selected piece
-                self.move_piece(self.selected_piece, position)
+            if square in self.valid_moves:
+                self.move_piece(self.selected_piece, square)
                 self.selected_piece = None
                 self.valid_moves = []
-    
-    def move_piece(self, piece, new_position):
-        current_row, current_col = piece.position
-        new_row, new_col = new_position
 
-        # Check if the clicked position is a valid move for the selected piece
-        if new_position in self.valid_moves:
-            self.board[current_row][current_col] = None  # Remove the piece from the old position
-            self.board[new_row][new_col] = piece  # Place the piece in the new position
-            piece.position = new_position  # Update the piece's position
-            
-            self.turn = 'white' if self.turn == 'black' else 'black'
+    def move_piece(self, piece, target_square):
+        current_square = self.get_square_from_piece(piece)
 
-        # Deselect the piece after the move
-        self.selected_piece = None
-        self.valid_moves = []
+        if target_square in self.valid_moves:
+            current_square.occupying_piece = None
+            target_square.occupying_piece = piece
+            piece.position = target_square.pos
 
     def get_valid_moves(self, player_color):
         valid_moves = []
-        for row in range(8):
-            for col in range(8):
-                piece = self.board[row][col]
-                if piece is not None and piece.color == player_color:
-                    piece_valid_moves = piece.get_valid_moves(self.board)
-                    valid_moves.extend([(row, col, r, c) for r, c in piece_valid_moves])
-        print(f"Valid moves for {player_color}: {valid_moves}")  # Debug print
-
-
+        for square in self.squares:
+            if square.occupying_piece and square.occupying_piece.color == player_color:
+                piece_valid_moves = square.occupying_piece.get_valid_moves(self)
+                valid_moves.extend([s for s in self.squares if s.pos in piece_valid_moves])
         return valid_moves
+
+    def get_square_from_pos(self, position):
+        for square in self.squares:
+            if square.pos == position:
+                return square
+
+    def get_square_from_piece(self, piece):
+        for square in self.squares:
+            if square.occupying_piece == piece:
+                return square
--- chess_data/pieces/pawn.py
+++ chess_data/pieces/pawn.py
@@ -2,9 +2,10 @@ import pygame
 import os
 
 class Pawn:
-    def __init__(self, color, pos):
+    def __init__(self, color, pos, board):
         self.color = color
         self.position = pos
+        self.board = board  # Reference to the board
         # Construct the file path to the image dynamically
         image_folder = os.path.join('chess_assets', 'images', 'imgs-80px')
         image_filename = f'{color}_pawn.png'
@@ -13,44 +14,48 @@ class Pawn:
         # Load the image
         self.image = pygame.image.load(image_path)
         self.has_moved = False
-    
+
+    def draw(self, display, x, y):
+        # Draw the pawn piece at the specified position (x, y) on the display
+        display.blit(self.image, (x, y))
+
     def get_image(self):
         return self.image
-    
-    def capture(self, target_position, board):
-        row, col = self.position
-        target_row, target_col = target_position
 
-        # Check if the target position is a valid capture
-        if abs(target_row - row) == 1 and abs(target_col - col) == 1:
-            target_piece = board[target_row][target_col]
-
-            if target_piece is not None and target_piece.color != self.color:
-                board[target_row][target_col] = None
-                self.move(target_position, board)
-                return True
+    def move(self, new_position):
+        # Implement the move logic for the pawn
+        # Ensure that the move is valid before updating the position
+        if self.is_valid_move(new_position):
+            self.position = new_position
+            self.has_moved = True
     
     def get_valid_moves(self, board):
         valid_moves = []
         row, col = self.position
-        
+
         if self.color == 'white':
-        # Move one square forward
+            # Move one square forward if empty
             if row > 0 and board[row - 1][col] is None:
                 valid_moves.append((row - 1, col))
+                if not self.has_moved and board[row - 2][col] is None:
+                    valid_moves.append((row - 2, col))
+            
             # Capture diagonally
             if row > 0 and col > 0 and board[row - 1][col - 1] is not None and board[row - 1][col - 1].color == 'black':
                 valid_moves.append((row - 1, col - 1))
             if row > 0 and col < 7 and board[row - 1][col + 1] is not None and board[row - 1][col + 1].color == 'black':
                 valid_moves.append((row - 1, col + 1))
         elif self.color == 'black':
-            # Move one square forward
+            # Move one square forward if empty
             if row < 7 and board[row + 1][col] is None:
                 valid_moves.append((row + 1, col))
+                if not self.has_moved and board[row + 2][col] is None:
+                    valid_moves.append((row + 2, col))
+            
             # Capture diagonally
             if row < 7 and col > 0 and board[row + 1][col - 1] is not None and board[row + 1][col - 1].color == 'white':
                 valid_moves.append((row + 1, col - 1))
             if row < 7 and col < 7 and board[row + 1][col + 1] is not None and board[row + 1][col + 1].color == 'white':
                 valid_moves.append((row + 1, col + 1))
-
+        
         return valid_moves
